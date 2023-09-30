c609c68fdd6140e1109a9ed5337d9913266651c1
Amir Sharapov <amir@amirfounder.com>
2023-09-30 01:32:02 -0500

    a catch all to log all events

--- pycdp/logging.py
+++ pycdp/logging.py
@@ -1,13 +1,20 @@
 from typing import Literal
 
 LoggableEvents = Literal[
+    '*',
     'connection.handle_message',
     'connection.handle_event',
     'connection.handle_response',
     'connection.send'
 ]
 
-events_to_log: dict[LoggableEvents, bool] = {}
+events_to_log: dict[LoggableEvents, bool] = {
+    '*': False,
+    'connection.handle_message': False,
+    'connection.handle_event': False,
+    'connection.handle_response': False,
+    'connection.send': False
+}
 
 
 def enable_logging(
@@ -31,7 +38,4 @@ def disable_logging(
 def is_logging_enabled(
         event: LoggableEvents
 ):
-    return events_to_log.get(
-        event,
-        False
-    )
+    return events_to_log['*'] or events_to_log[event]
